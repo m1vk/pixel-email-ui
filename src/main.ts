b3243a465965fdfee99cedf8e9a862c4e229fa3b
microzavrik <microzavrik7@gmail.com>
2024-11-15 23:29:24 +0300

    feat: add main.ts settings

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,14 @@
+import { DefaultApolloClient, provideApolloClient } from '@vue/apollo-composable'
 import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
 
-createApp(App).mount('#app')
+import App from './App.vue'
+import router from './router'
+import { apolloClient } from './services/backend/connect-graphql.ts'
+
+const app = createApp(App)
+
+provideApolloClient(apolloClient)
+app.provide(DefaultApolloClient, apolloClient)
+
+app.use(router)
+app.mount('#app')
