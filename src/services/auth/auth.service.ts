39cb36095afd2c4e5933e9a5506656b43f93eb38
microzavrik <microzavrik7@gmail.com>
2024-11-15 23:28:46 +0300

    feat: add backend connection && auth service

---
+++ src/services/auth/auth.service.ts
@@ -0,0 +1,17 @@
+export class AdminAuthService {
+    static setAuthToken(token: string) {
+        localStorage.setItem('access_token', token)
+    }
+
+    static getAuthToken() {
+        return localStorage.getItem('access_token')
+    }
+
+    static hasAuthToken() {
+        return !!localStorage.getItem('access_token')
+    }
+
+    static removeAuthToken() {
+        localStorage.removeItem('access_token')
+    }
+}
---
+++ src/services/backend/make-link.ts
@@ -0,0 +1,32 @@
+const getProtocol = (port: string): string => {
+    switch (port) {
+        case '80':
+            return 'http:'
+        case '443':
+            return 'https:'
+        default:
+            return window.location.protocol
+    }
+}
+
+export const BaseHOSTHttp = (() => {
+    const isDev = import.meta.env.MODE === 'development'
+    if (isDev) {
+        const url = import.meta.env.VITE_API_URL
+        if (typeof url !== 'string') {
+            throw new Error('Invalid api url')
+        }
+
+        return url
+    }
+
+    const win: any = window
+
+    const port = win.BACKEND_PORT ?? window.location.port
+    const hostname = win.BACKEND_HOST ?? window.location.hostname
+    const protocol = getProtocol(String(port))
+
+    return port === '0' ? `${protocol}//${hostname}` : `${protocol}//${hostname}:${port}`
+})()
+
+export const BaseHOSTWs = (() => BaseHOSTHttp.replace('http', 'ws'))()
